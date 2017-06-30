a5fd679b7825d90989e7eb645f47c47d894797e5
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-06-30 10:10:11 +0200

    Add test to check if the application is running.

--- src/test/java/nl/ekholabs/microservices/geoip/GeoipApplicationTests.java
+++ src/test/java/nl/ekholabs/microservices/geoip/GeoipApplicationTests.java
@@ -2,15 +2,28 @@ package nl.ekholabs.microservices.geoip;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
 public class GeoipApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	public void contextLoads() {
+	public void statusOk() throws Exception {
+		final RequestBuilder request = get("/ipservice");
+		mvc.perform(request).andExpect(status().is4xxClientError());
 	}
 
 }
